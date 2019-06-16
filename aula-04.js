c6098dec3848634852117a748f2cb3ddc49ccff8
Eduardo Marques <edu.temarques@gmail.com>
2019-06-16 18:55:09 -0300

    Atualização aula 04

---
+++ aula-04.js
@@ -0,0 +1,27 @@
+// Truthy e Falsy
+console.log('Truthy e Falsy');
+console.log('--------------');
+
+console.log();
+
+console.log('Falsy');
+console.log('Representam false: ');
+console.log('false');
+console.log('undefined');
+console.log('null');
+console.log('0');
+console.log('-0');
+console.log('String Vazia');
+
+console.log();
+
+console.log('Truthy');
+console.log('Representam true: ');
+console.log('Todo restante');
+
+// Operador Ternário
+console.log('Ternário');
+console.log('--------------');
+
+console.log();
+
