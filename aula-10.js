e7676805d9df6b018839aa4e5e692cc0e1b51c51
José Eduardo Trindade E Marques <jose.eduardo.marques@accenture.com>
2019-06-19 17:11:16 -0300

    Finalização aula 10

---
+++ aula-10.js
@@ -0,0 +1,38 @@
+// Object Wrapper
+console.log('Object Wrapper');
+console.log('--------------');
+console.log();
+
+console.log('Exemplo 1');
+console.log('Declarando variável string');
+var nome = 'Eduardo';
+console.log('Acessando tamanho da variável string: ', nome.length);
+console.log();
+
+// Constructors
+console.log('Constructor');
+console.log('-----------');
+console.log();
+
+console.log('Exemplo 1');
+console.log('Declarando variável string com constructor');
+var nomeConstrutor  = new String('Eduardo');
+console.log('Acessando tamanho da variável string com constructor: ', nomeConstrutor.length);
+console.log();
+
+// Typeof
+console.log('Typeof');
+console.log('------');
+console.log();
+
+console.log('Exemplo 1');
+console.log('Typeof String: ', typeof 'Eduardo');
+console.log('Exemplo 2');
+console.log('Typeof Number: ', typeof 1);
+console.log('Exemplo 3');
+console.log('Typeof Boolean: ', typeof true);
+console.log('Exemplo 4');
+console.log('Typeof undefined: ', typeof undefined);
+
+
+
