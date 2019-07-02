40f9a8e092a2cf30ea04bed0d53c969e4780d874
José Eduardo Trindade E Marques <jose.eduardo.marques@accenture.com>
2019-07-02 14:00:27 -0300

    Finalização aula 20

--- aula-20.js
+++ aula-20.js
@@ -10,19 +10,19 @@ console.log();
 // Alert
 console.log('Alert');
 console.log('-----');
-alert('Isso é um alerta');
+// alert('Isso é um alerta');
 console.log();
 
 // Prompt
 console.log('Prompt');
 console.log('------');
-prompt('Isso é um prompt?');
+// prompt('Isso é um prompt?');
 console.log();
 
 // Confirm
 console.log('Confirm');
 console.log('-------');
-prompt('Isso é um confirm?');
+// confirm('Isso é um confirm?');
 console.log();
 
 // D.O.M
@@ -32,16 +32,44 @@ console.log();
 
 console.log('GetElementById');
 console.log('--------------');
-console.log(document.getElementById('my-link'))
+// console.log(document.getElementById('my-link'));
 console.log();
 
 
 console.log('GetElementByClassName');
 console.log('---------------------');
-console.log(document.getElementsByClassName('my-class"'))
+// console.log(document.getElementsByClassName('my-class'));
 console.log();
 
 console.log('GetElementByTagName');
 console.log('-------------------');
-console.log(document.getElementsByTagName('h4"'))
+// console.log(document.getElementsByTagName('h4'));
 console.log();
+
+console.log('GetElementByName');
+console.log('----------------');
+// console.log(document.getElementsByName('username'));
+console.log();
+
+console.log('QuerySelector');
+console.log('-------------');
+console.log(document.querySelector('input'));
+console.log();
+
+console.log('QuerySelectorAll');
+console.log('----------------');
+console.log(document.querySelectorAll('input'));
+console.log();
+
+console.log('Input Values');
+console.log('------------');
+var $inputPassword = document.querySelector('#password');
+var $inputUsername = document.querySelector('#username');
+console.log($inputUsername.value, $inputPassword.value);
+
+console.log('Eventos');
+console.log('-------');
+var $button = document.querySelector('button').addEventListener('click', function(event) {
+    event.preventDefault();
+    console.log('Clique no Botão');
+}, false);
